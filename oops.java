65aaae84dfc6c3ddf45571895b39ad0ee547ff8a
kanhaiya yadav <163013742+Kanhaiyadav01@users.noreply.github.com>
2026-02-02 19:42:53 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,113 @@
+// import java.security.PublicKey;
+
+// class Vehicle{
+//     void start(){
+//         System.out.println("vehicle starts");
+//     }
+// }
+
+
+// single inheritance 
+// class Bike extends Vehicle{
+//     void drive(){
+//         System.out.println("Bike running");
+//     }
+// }
+
+//  class Tvs extends Bike{
+//     void price(){
+//         System.out.println("1.5 lakh");
+//     }
+
+// }
+
+
+// multilevel inheritance
+// class FourWheeler extends Vehicle{
+//     FourWheeler(){
+//         System.out.println("FourWheeler");
+//     }
+// }
+
+// class Car extends FourWheeler{
+//     Car(){
+//         System.out.println("car");
+//     }
+// }
+
+
+// multiple inheritance
+// interface tv{
+//     void WatchingTv();
+
+// }
+
+// interface food{
+//     void EatingFood();
+// }
+
+// class Student implements tv , food {
+//     public void WatchingTv(){
+//          System.out.println("WatchingTv");
+//     }
+
+//     public void EatingFood(){
+//         System.out.println("EatingFood");
+//     }
+// }
+
+
+// inheriting const using super
+// class Person{
+//     Person(String name){
+//         System.out.println("person name :"+ name);
+//     }
+// }
+
+// class CollageStudent extends Person{
+//     CollageStudent(String name){
+//        super(name); // parent class const called
+//         System.out.println("collage student const called");
+//     }
+// }
+
+// inheriting attributes
+// class University {
+//     void task(){
+//         System.out.println("pani");
+//     }
+//     String universityName = "IILM University";
+// }
+
+// class Collage extends University {
+//     void task(){
+//         System.out.println("water");
+//     }
+//     void displayUniversityName(){
+//         System.out.println("University: " + universityName);
+//     }
+// }
+
+
+
+public class oops {
+    public static void main(String args[]){
+       // Bike b = new Bike();
+       // Tvs t = new Tvs();
+        // b.start();
+        // b.drive();
+       // t.price();
+      // Car obj = new Car();
+      //    Student s = new Student();
+     //    s.WatchingTv();
+     //    s.EatingFood();
+
+     //CollageStudent std = new CollageStudent("Kanhaiya");
+
+     //  Collage obj = new Collage();
+     //  obj.task();
+
+       
+    }
+    
+}
